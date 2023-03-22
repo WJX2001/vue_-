07d557c960882e9d1c21edb28f7a69049e63e467
WJX2001 <1758807220@qq.com>
2023-03-22 19:23:17 +0800

    feat: vue初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入elementUi
+import ElementUI from 'element-ui';
+// 引入elementUi css
+import 'element-ui/lib/theme-chalk/index.css';
+// 安装到vue应用中
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
