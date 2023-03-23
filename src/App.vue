4a3bb62574d9d9e097dc8bdc0b328ecefbdb47e9
WJX2001 <1758807220@qq.com>
2023-03-23 13:26:54 +0800

    props数据流

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,11 @@
         loop></video>
     </div>
     <div class="container">
+      <!-- <ChildComp></ChildComp> -->
       <div class="desc"></div>
       <div class="login">
         <div class="login-wrapper">
+          <child-comp v-if="isShowChild" :info="pInfo"></child-comp>
           <el-form :model="form" label-width="80px">
             <el-form-item label="用户名：">
               <el-input v-model="form.name"></el-input>
@@ -27,7 +29,13 @@
 </template>
 
 <script>
+// 引入
+import ChildComp from './components/ChildComp.vue';
 export default {
+  // components
+  components: {
+    ChildComp,
+  },
   // 预定义属性
   name: 'App',
   // 组件当中所有的响应式数据
@@ -35,8 +43,10 @@ export default {
     return {
       form: {
         name: '',
-        passwd: ''
+        passwd: '',
       },
+      isShowChild: true,
+      pInfo: '这是用来设置默认的input框的值'
     }
   },
   // 方法
@@ -70,9 +80,15 @@ export default {
   mounted() {
     // 当 当前组件 被挂载时，出发执行
     setTimeout(() => {
-      this.isShow = false,
-        this.isHighLight = true
+      this.isShow = false;
+      this.isHighLight = true;
     }, 3000)
+    // setInterval(() => {
+    //   this.isShowChild = !this.isShowChild;
+    // }, 3000)
+    setTimeout(() => {
+      this.pInfo = '我的值变化了'
+    }, 5000)
   },
 }
 </script>
@@ -86,6 +102,7 @@ html,
 body {
   margin: 0;
   padding: 0;
+  overflow-y: hidden;
 }
 .container {
   display: flex;
@@ -116,3 +133,5 @@ body {
   }
 }
 </style>
+
+
--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,10 +1,111 @@
 <template>
-  <div></div>
+  <div>
+    <!-- <el-input v-model="message"></el-input>
+    <el-input v-model="name"></el-input> -->
+    <div>这是反转之后的值{{ reverseMsg }}</div>
+    <!-- 如果info 为空，就展示当前父组件没有传递数据，不为空，正常展示-->
+    <div>{{ info || '当前父组件没有传数据给我'}}</div>
+    <!-- <div v-if="info">{{ info }}</div>
+    <div v-else>父组件当前没有给我传数据</div> -->
+
+    <!-- <div>{{ wrapperInfo }}</div> -->
+    <el-input v-model="localInfo"></el-input>
+    <el-input v-model="localCompute"></el-input>
+    <el-button @click="monitor">查看值</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ChildComp'
+  name: "ChildComp",
+  props: {
+    info:String,
+  },
+
+  // 生命周期函数
+  // 1.组件实例被创建之初
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  // 2.组件实例已经完全创建
+  created() {},
+  // 3.组件挂载之前
+  beforeMount() {},
+  // 4.组件挂载到实例上之后
+  mounted() {
+    window.addEventListener("click", this.handleEvent);
+  },
+  // 5.组件数据发生变化，更新之前
+  beforeUpdate() {},
+  // 6.组件数据更新之后
+  updated() {},
+  // 7.组件实例销毁之前
+  beforeDestroy() {},
+
+  // 8.组件实例销毁之后
+  destroyed() {
+    // 取消事件订阅
+    console.log("destroyed");
+    window.removeEventListener("click", this.handleEvent);
+  },
+
+  data: function () {
+    return {
+      message: "",
+      name: '',
+      localInfo:'',
+      localComputeRealValue: ''
+    };
+  },
+
+  methods: {
+    handleEvent() {
+      console.log("hahaha");
+    },
+    monitor() {
+      console.log(this.localCompute,this.localComputeRealValue)
+    }
+  },
+
+  // 计算属性
+  computed: {
+    reverseMsg: function () {
+      return this.message.split("").reverse().join("");
+    },
+    wrapperInfo: function() {
+      this.info? this.info: '当前父组件没有给我传值'
+    },
+    // 做一个数据代理
+    localCompute: {
+      get: function() {
+        return this.localComputeRealValue || this.info
+      },
+      set: function(v) {
+        this.localComputeRealValue = v
+      }
+    }
+  },
+
+  watch: {
+    name: function(newValue,oldValue){
+        console.log(newValue,oldValue)
+    },
+    message: {
+        handler: function(newValue,oldValue){
+            console.log(newValue,oldValue)
+        },
+        immediate:true
+    },
+    info: {
+            handler: function (newValue, oldValue) {
+                console.log('info', newValue, oldValue)
+                this.localInfo = newValue
+            },
+            immediate: true
+        }
+  
+    
+  }
 };
 </script>
 
